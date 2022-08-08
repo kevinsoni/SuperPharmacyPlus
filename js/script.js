9ecd662cab54a894533a0a317f18bf5904742a0d
kevinsoni <kevinsoni22593@gmail.com>
2022-08-08 16:03:47 -0700

    1366

--- js/script.js
+++ js/script.js
@@ -3,14 +3,37 @@ $().ready(function () {
         arrows: false,
         centerPadding: "0",
         dots: false,
-        // slidesToShow: ,
         infinite: true,
-        autoplay: false,
-        autoplaySpeed: 3000,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
         fade: true,
         centerMode: true,
         adaptiveHeight: true,
         focusOnSelect: true,
+        prevArrow: '<i class="fa-solid fa-angle-left next"></i>',
+        nextArrow: '<i class="fa-solid fa-angle-right previous"></i>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    prevArrow: '<i class="fa-solid fa-angle-left next"></i>',
+                    nextArrow: '<i class="fa-solid fa-angle-right previous"></i>',
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    prevArrow: '<i class="fa-solid fa-angle-left next"></i>',
+                    nextArrow: '<i class="fa-solid fa-angle-right previous"></i>',
+                }
+            }
+        ]
     });
 });
 
@@ -20,43 +43,95 @@ $(document).ready(function () {
     });
 });
 
-$('.productcard').slick({
+
+$('.usp').slick({
     dots: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
+    // centerPadding: "0",
+    // centerMode: true,
     arrows: false,
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     // autoplay: true,
-    // autoplaySpeed: 2000,
+    // autoplaySpeed: 3000,
     responsive: [
         {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: false
-            }
+            breakpoint: 2000,
+            settings: "unslick"
         },
         {
-            breakpoint: 768,
+            breakpoint: 1440,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1024,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 769,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 426,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
         }
     ]
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
+});
+
+
+
+$('.productcard').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    // slidesToShow: 4,
+    // slidesToScroll: 1,
+    arrows: false,
+    focusOnSelect: true,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 2000,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1440,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1024,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true,
+                prevArrow: '<i class="fa-solid fa-angle-left brandnext"></i>',
+                nextArrow: '<i class="fa-solid fa-angle-right brandprevious"></i>',
+            }
+        },
+        {
+            breakpoint: 426,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                prevArrow: '<i class="fa-solid fa-angle-left brandnext"></i>',
+                nextArrow: '<i class="fa-solid fa-angle-right brandprevious"></i>',
+            }
+        }
+    ]
 });
 
 
@@ -65,41 +140,68 @@ $('.brands').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 5,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
+    focusOnSelect: true,
     arrows: false,
     // autoplay: true,
-    // autoplaySpeed: 2000,
+    // autoplaySpeed: 3000,
     responsive: [
         {
-            breakpoint: 1280,
+            breakpoint: 2000,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1440,
+            settings: "unslick"
+        },
+        // {
+        //     breakpoint: 1280,
+        //     settings: {
+        //         slidesToShow: 4,
+        //         slidesToScroll: 4,
+        //         infinite: true,
+        //         dots: false,
+        //         arrows: false,
+        //     }
+        // },
+        {
+            breakpoint: 1025,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 4,
-                infinite: true,
-                dots: false
-            }
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: false
+                dots: false,
+                arrows: false,
             }
         },
         {
-            breakpoint: 768,
+            breakpoint: 769,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 2,
+                arrows: true,
+                prevArrow: '<i class="fa-solid fa-angle-left brandnext"></i>',
+                nextArrow: '<i class="fa-solid fa-angle-right brandprevious"></i>',
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 601,
             settings: {
+                arrows: true,
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                prevArrow: '<i class="fa-solid fa-angle-left brandnext"></i>',
+                nextArrow: '<i class="fa-solid fa-angle-right brandprevious"></i>',
+            }
+        },
+        {
+            breakpoint: 426,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow: '<i class="fa-solid fa-angle-left brandnext"></i>',
+                nextArrow: '<i class="fa-solid fa-angle-right brandprevious"></i>',
             }
         }
     ]
